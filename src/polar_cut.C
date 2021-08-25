ab4d5e06bf0897bb7edd074043595fa82ff5de0a
Maximilian Köper <>
2021-08-25 17:34:58 +0200

    edit ploar cut

--- src/polar_cut.C
+++ src/polar_cut.C
@@ -1,5 +1,3 @@
-#include "TGraphPolar.h"
-
 #define R_INNER1_DET 23.0
 #define W_INNER1_DET 120.0
 
@@ -10,45 +8,47 @@
 #define W_OUTER1_DET 360.0
 
 void polar_cut(){
-    TCanvas * CPol = new TCanvas("CPol","TGraphPolar Example",500,500);
+    TCanvas * CPol = new TCanvas("CPol","TGraphPolar Example",1000,1000);
 
-    Double_t theta[8];
-    Double_t radius[8];
-    Double_t etheta[8];
-    Double_t eradius[8];
-    
-    for (int i=0; i<8; i++) {
-        theta[i]   = (i+1)*(TMath::Pi()/4.);
-        radius[i]  = (i+1)*0.05;
-    }
-    
-    TGraphPolar * grP1 = new TGraphPolar(8, theta, radius);
-    grP1->SetTitle("Detector Cut");
+    CPol->cd();
+
+    Double_t theta[] = {0, 0.1,0.2,0.5};
+    Double_t radius[] = {0, 23, 29, 70};
+    Double_t z[] = {0, 50, 60, 100};
+
+
 
     //Draw detector layers
-    TEllipse *c1 = new TEllipse(0,0,R_INNER1_DET, R_INNER1_DET);
+    TEllipse *c1 = new TEllipse(0,0, 0.70, 0.70);
+    c1->SetLineColor(kRed+3);
     c1->Draw();
-    TEllipse *c2 = new TEllipse(0,0,R_INNER2_DET, R_INNER2_DET);
-    c2->Draw("Same");
-    TEllipse *c3 = new TEllipse(0,0,R_OUTER1_DET, R_OUTER1_DET);
-    c3->Draw("Same");
-    
+    TEllipse *c2 = new TEllipse(0,0,0.29, 0.29);
+    c2->SetLineColor(kRed+3);
+    c2->Draw("same");
+    TEllipse *c3 = new TEllipse(0,0,0.23, 0.23);
+    c3->SetLineColor(kRed+3);
+    c3->Draw("same");
+
+    TGraphPolar * grP1 = new TGraphPolar(4, theta, radius);
+    grP1->SetTitle("Detector Cut");
+
+
     //setting style
     grP1->SetMarkerStyle(20);
-    grP1->SetMarkerSize(2.);
+    grP1->SetMarkerSize(2);
     grP1->SetMarkerColor(4);
     grP1->SetLineColor(2);
     grP1->SetLineWidth(3);
-    grP1->Draw("Psame");
-    
+    //grP1->SetFillStyle(0);
+    grP1->Draw("CP same");
+
     // Update, otherwise GetPolargram returns 0
     CPol->Update();
     grP1->GetPolargram()->SetToRadian();
     grP1->GetPolargram()->SetLineWidth(0);
     grP1->GetPolargram()->SetRangeRadial(0,100);
-    
-    //CPol->Update();
-    
-    return CPol;
-}
 
+    //CPol->Update();
+
+    return;
+}
